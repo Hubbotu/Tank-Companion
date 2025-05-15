9250bc42d519ecef6321991af1fdcb058b99f954
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-05-15 16:49:10 +0300

    Add files via upload

---
+++ ruRU.lua
@@ -0,0 +1,11 @@
+-- Translator ZamestoTV
+local L = LibStub("AceLocale-3.0"):NewLocale("TankCompanion", "ruRU")
+if not L then return end
+
+L.ADVANCED_INTEGRATIONS = "Расширенные интеграции"
+L.DETAILS_PLUGIN        = "Включить плагин Details!"
+L.PERFORMANCE_MONITOR   = "Включить монитор производительности"
+L.RAID_PROFILES         = "Включить профили для рейдов и подземелий"
+L.COMBAT_CHARTS         = "Включить графики времени боя"
+L.MINIMAP               = "Значок на миникарте"
+L.SHOW_MINIMAP_ICON     = "Показывать значок на миникарте"
